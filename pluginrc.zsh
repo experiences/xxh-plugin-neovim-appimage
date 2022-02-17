1cceda3a2b0b0853de0f0093196a36bea325f6a2
experience <m9experiene@gmail.com>
2022-02-17 10:57:21 +0800

    init

--- pluginrc.zsh
+++ pluginrc.zsh
@@ -4,7 +4,7 @@
 # Put here your functions, environment variables, aliases and whatever you need.
 
 CURR_DIR="$(cd "$(dirname "$0")" && pwd)"
-plugin_name='xxh-plugin-neovim-appimage'
+plugin_name='xxh-plugin-zsh-neovim-appimage'
 
 export PATH=$CURR_DIR/:$PATH
 alias vim='nvim.appimage'
